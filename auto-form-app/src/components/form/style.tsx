324d9a58fb68a8f461cf1346fa41eebaf040858e
Zabdiel-Rosas <zabdiel_rosas@outlook.com>
2021-07-27 17:22:27 -0500

    scroll behavior is changed and color variables are implemented

--- auto-form-app/src/components/form/style.tsx
+++ auto-form-app/src/components/form/style.tsx
@@ -1,35 +1,39 @@
-import styled from 'styled-components'
-const themeColor = '#FFA463'
+import styled from 'styled-components';
+const themeColor = '255, 164, 99'; // this variable must have a RGB value
+const white = '#FFFFFF';
 
 // Typography Section
 export const Text = styled.p`
 font-size: 25px;
-color: white;
+color: ${white};
 `
 export const Label = styled.label`
-color: #FFFFFF;
+color: ${white};
 `
 
 // Containers Section
 
 // Background Container
-export const  Test = styled.form`
+export const  Container = styled.form`
 background-image: url('https://wallpaperaccess.com/full/84248.png');
 background-size: cover;
 height: 100vh;
-overflow: auto;
+overflow-y: scroll;
+scroll-snap-type: y mandatory;
+flex-flow: column nowrap;
 `
 // Tile Container
-export const Container = styled.div`
+export const Tile = styled.section`
 background: transparent;
 height: 100vh;
 display: flex;
 justify-content: center;
 align-items: center;
+scroll-snap-align: start;
 `
 
 // Question Container
-export const  QContainer = styled.div`
+export const  Question = styled.div`
 
 width: 40%;
 height: 20%;
@@ -57,19 +61,20 @@ export const OptContainer = styled.div<OptContainerProps>`
 width: ${props => props.size > 3 ? '50%' : '25%' };
 margin-bottom: 10px;
 padding: 15px 30px 15px 0;
-border: 3px ${themeColor} solid;
+border: 3px rgba(${themeColor}) solid;
 border-radius: 5px;
-background-color: rgba(255, 164, 99, .3);
+background-color: rgba(${themeColor}, .3);
 `
 
 // Select Container
 export const Select = styled.select`
 width: 50%;
 font-size: 18px;
-background-color: rgba(255, 164, 99, .3);
-border: 3px ${themeColor} solid;
+background-color: rgba(${themeColor}, .3);
+border: 3px rgba(${themeColor}) solid;
 border-radius: 5px;
-color: #FFFFFF;
+color: ${white};
+padding: 5px 0;
 :focus {
         outline: none;
     }
@@ -81,25 +86,25 @@ export const Input = styled.input`
 width: 100%;
 font-size: 25px;
 border: none;
-color: ${themeColor};
-border-bottom: 2px solid rgba(255, 164, 99, .5);
+color: rgba(${themeColor});
+background-color: rgba(${themeColor}, .5);
 background: transparent;
 padding-bottom: 10px;
 :focus {
         outline: none;
-        border-bottom: 2px solid ${themeColor} ;
+        border-bottom: 2px solid rgba(${themeColor}) ;
     }
 ::placeholder {
-    color: rgba(255, 164, 99, .5);
+    color: rgba(${themeColor}, .5);
 }
 `
 export const Date = styled.input`
 font-size: 25px;
 width: 45%;
-color: #FFFFFF;
+color: ${white};
 padding: 10px;
-background: rgba(255, 164, 99, .5);
-border: 3px ${themeColor} solid;
+background-color: rgba(${themeColor}, .5);
+border: 3px rgba(${themeColor}) solid;
 border-radius: 5px;
 :focus {
         outline: none;
@@ -111,7 +116,7 @@ border-radius: 5px;
 
 // Select option
 export const Option = styled.option`
-background-color: rgba(255, 164, 99, .9);
-color: white;
+background-color: ${white};
+color: #000000;
 padding: 20px;
 `
